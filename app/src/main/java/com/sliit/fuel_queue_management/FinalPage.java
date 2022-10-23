f209949847aa45406e2b6f81bacdea295ce241c5
MenukaJ <menukaj@lolctech.com>
2022-10-23 23:23:43 +0530

    adde side nav

--- app/src/main/java/com/sliit/fuel_queue_management/FinalPage.java
+++ app/src/main/java/com/sliit/fuel_queue_management/FinalPage.java
@@ -1,6 +1,14 @@
 package com.sliit.fuel_queue_management;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.annotation.SuppressLint;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.view.MenuItem;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,18 +17,48 @@ import android.widget.TextView;
 public class FinalPage extends AppCompatActivity {
 
     TextView text;
+    public DrawerLayout drawerLayout;
+    public ActionBarDrawerToggle actionBarDrawerToggle;
+
     @Override
     public void onBackPressed() {
         FinalPage.this.finish();
     }
+
+    @SuppressLint("ResourceType")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getSupportActionBar().hide();
+        //getSupportActionBar().hide();
         setContentView(R.layout.activity_final_page);
+
+        drawerLayout = findViewById(R.id.my_drawer_layout);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.nav_open, R.string.nav_close);
+
+        // pass the Open and Close toggle for the drawer layout listener
+        // to toggle the button
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+
+        // to make the Navigation drawer icon always appear on the action bar
+        getSupportActionBar().setTitle("Home");
+        //getSupportActionBar().setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.drawable.background_main)));
+        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#A443E9")));
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        //setContentView(R.layout.activity_final_page);
         text = findViewById(R.id.changeText);
         Intent intent = getIntent();
         String s2 = intent.getStringExtra("email");
         text.setText(s2);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
