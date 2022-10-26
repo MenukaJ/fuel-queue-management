0678a9cef0b9f0ec8b3dfd7d9ee5c7bffccfd245
Miyuru Wijesinghe <MiyuruW@lolctech.com>
2022-10-26 22:10:33 +0530

    updated

---
+++ app/src/main/java/com/sliit/fuel_queue_management/FuelDetailsList.java
@@ -0,0 +1,14 @@
+package com.sliit.fuel_queue_management;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FuelDetailsList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fuel_details_list);
+    }
+}
