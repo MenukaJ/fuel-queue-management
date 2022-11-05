fea2e5cfeb78eeb8beed1d28fa3c3ebb2b3dc35b
Miyuru Wijesinghe <MiyuruW@lolctech.com>
2022-11-05 18:13:39 +0530

    updated

--- app/src/main/java/com/sliit/fuel_queue_management/model/FuelDetailsAdapter.java
+++ app/src/main/java/com/sliit/fuel_queue_management/model/FuelDetailsAdapter.java
@@ -5,6 +5,7 @@ import android.app.Dialog;
 import android.content.Context;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,13 +20,24 @@ import androidx.annotation.Nullable;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.NetworkResponse;
 import com.android.volley.Request;
+import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.HttpHeaderParser;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
+import com.sliit.fuel_queue_management.FuelDetailsList;
 import com.sliit.fuel_queue_management.R;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
@@ -34,6 +46,9 @@ public class FuelDetailsAdapter extends RecyclerView.Adapter<FuelDetailsAdapter.
     private Context context;
     private ArrayList<FuelDetails> fuelDetails;
     private SwipeRefreshLayout refresh;
+    private EditText fuelType, fuelArrival, fuelFinished, fuelStatus;
+    private TextView fuelStationName, fuelDate;
+    private FuelStation fuelStationClass = new FuelStation();
     private String url = "https://fuel-queue-management.herokuapp.com/api/FuelDetails";
 
     public FuelDetailsAdapter(Context context, ArrayList<FuelDetails> fuelDetails) {
@@ -65,7 +80,8 @@ public class FuelDetailsAdapter extends RecyclerView.Adapter<FuelDetailsAdapter.
                 String date = fuelDetails.get(position).getDate();
                 String status = fuelDetails.get(position).getStatus();
                 String stationName = fuelDetails.get(position).getFuelStation().getName();
-                editFuelDetails(id, type, arrivalTime, finishTime, date, status, stationName);
+                fuelStationClass = fuelDetails.get(position).getFuelStation();
+                editFuelDetails(id, type, arrivalTime, finishTime, date, status, stationName, fuelStationClass);
             }
         });
         holder.deleteFuelDet.setOnClickListener(new View.OnClickListener() {
@@ -77,10 +93,8 @@ public class FuelDetailsAdapter extends RecyclerView.Adapter<FuelDetailsAdapter.
         });
     }
 
-    private void editFuelDetails(String id, String pType, String pArrival, String pFinish, String pDate, String pStatus, String pStation) {
+    private void editFuelDetails(String id, String pType, String pArrival, String pFinish, String pDate, String pStatus, String pStation, FuelStation pFuelStation) {
         TextView close, header;
-        final EditText fuelType, fuelArrival, fuelFinished, fuelStatus;
-        final TextView fuelStationName, fuelDate;
         Button submit;
         final Dialog dialog;
 
@@ -115,8 +129,43 @@ public class FuelDetailsAdapter extends RecyclerView.Adapter<FuelDetailsAdapter.
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String data = fuelType.getText().toString();
-                Submit("PUT", data, dialog, id);
+                if (fuelType.getText().toString().isEmpty()) {
+                    Toast.makeText(context.getApplicationContext(), "Fuel Type is required.", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (fuelArrival.getText().toString().isEmpty()) {
+                    Toast.makeText(context.getApplicationContext(), "Arrived time is required.", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (!fuelArrival.getText().toString().matches("^$|(\\d{2}):(\\d{2})$")) {
+                    Toast.makeText(context.getApplicationContext(), "Arrived time should be in HH:mm pattern.", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (fuelFinished.getText().toString().isEmpty()) {
+                    Toast.makeText(context.getApplicationContext(), "Finished time is required.", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (!fuelFinished.getText().toString().matches("^$|(\\d{2}):(\\d{2})$")) {
+                    Toast.makeText(context.getApplicationContext(), "Finished time should be in HH:mm pattern.", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (fuelDate.getText().toString().isEmpty()) {
+                    Toast.makeText(context.getApplicationContext(), "Date is required.", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (fuelStatus.getText().toString().isEmpty()) {
+                    Toast.makeText(context.getApplicationContext(), "Status is required.", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (!fuelStatus.getText().toString().matches("^$|Available|Finished")) {
+                    Toast.makeText(context.getApplicationContext(), "Fuel status is should be Available or Finished.", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (fuelStationName.getText().toString().isEmpty()) {
+                    Toast.makeText(context.getApplicationContext(), "Station name is required.", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                updateData(fuelType.getText().toString(), fuelArrival.getText().toString(), fuelFinished.getText().toString(), fuelDate.getText().toString(), fuelStatus.getText().toString(), pFuelStation, dialog, id);
             }
         });
 
@@ -147,7 +196,7 @@ public class FuelDetailsAdapter extends RecyclerView.Adapter<FuelDetailsAdapter.
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Submit("DELETE", "", dialog, id);
+                deleteData(dialog, id);
             }
         });
 
@@ -155,51 +204,95 @@ public class FuelDetailsAdapter extends RecyclerView.Adapter<FuelDetailsAdapter.
         dialog.show();
     }
 
-    private void Submit(String method, final String data, final Dialog dialog, final String id) {
-        if (method == "PUT") {
-            StringRequest request = new StringRequest(Request.Method.PUT, url + "/" + id, new Response.Listener<String>() {
+    private void updateData(String pFuelType, String pFuelArrival, String pFuelFinished, String pFuelDate, String pFuelStatus, FuelStation fuelStationObject, Dialog dialog, String id) {
+        try {
+            RequestQueue requestQueue = Volley.newRequestQueue(context.getApplicationContext());
+            JSONObject jsonBody = new JSONObject();
+            jsonBody.put("id", id);
+            jsonBody.put("type", pFuelType);
+            jsonBody.put("arrivalTime", pFuelArrival);
+            jsonBody.put("finishTime", pFuelFinished);
+            jsonBody.put("date", pFuelDate);
+            jsonBody.put("status", pFuelStatus);
+
+            String fuelStat = new Gson().toJson(fuelStationObject);
+            JSONObject fuelStatJob = new JSONObject(fuelStat);
+            jsonBody.put("fuelStation", fuelStatJob);
+            final String requestBody = jsonBody.toString();
+            Log.i("VOLLEY", requestBody);
+
+            StringRequest stringRequest = new StringRequest(Request.Method.PUT, url + "/" + id, new Response.Listener<String>() {
                 @Override
                 public void onResponse(String response) {
+                    Log.i("VOLLEY", response);
                     dialog.dismiss();
-                    Toast.makeText(context, "Record updated successfully", Toast.LENGTH_LONG).show();
-                }
-            }, new Response.ErrorListener() {
-                @Override
-                public void onErrorResponse(VolleyError error) {
-                    Toast.makeText(context, "Error occurred", Toast.LENGTH_LONG).show();
-                }
-            }) {
-                @Nullable
-                @Override
-                protected Map<String, String> getParams() {
-                    Map<String, String> params = new HashMap<>();
-                    params.put("cat", data);
-                    params.put("id", id);
-                    return params;
-                }
-            };
-            Volley.newRequestQueue(context).add(request);
-        } else if (method == "DELETE") {
-            StringRequest request = new StringRequest(Request.Method.DELETE, url + "/" + id, new Response.Listener<String>() {
-                @Override
-                public void onResponse(String response) {
-                    dialog.dismiss();
-                    refresh.post(new Runnable() {
+                    /*refresh.post(new Runnable() {
                         @Override
                         public void run() {
                             fuelDetails.clear();
+                            //getData();
                         }
-                    });
-                    Toast.makeText(context, "Record deleted successfully", Toast.LENGTH_LONG).show();
+                    });*/
+                    Toast.makeText(context.getApplicationContext(), "Record updated successfully", Toast.LENGTH_LONG).show();
                 }
             }, new Response.ErrorListener() {
                 @Override
                 public void onErrorResponse(VolleyError error) {
-                    Toast.makeText(context, "Error occurred", Toast.LENGTH_LONG).show();
+                    Log.i("VOLLEY", error.toString());
+                    Toast.makeText(context.getApplicationContext(), "Error occurred", Toast.LENGTH_LONG).show();
                 }
-            });
-            Volley.newRequestQueue(context).add(request);
+            }) {
+                @Override
+                public String getBodyContentType() {
+                    return "application/json; charset=utf-8";
+                }
+
+                @Override
+                public byte[] getBody() throws AuthFailureError {
+                    try {
+                        return requestBody == null ? null : requestBody.getBytes("utf-8");
+                    } catch (UnsupportedEncodingException uee) {
+                        VolleyLog.wtf("Unsupported Encoding while trying to get the bytes of %s using %s", requestBody, "utf-8");
+                        return null;
+                    }
+                }
+
+                @Override
+                protected Response<String> parseNetworkResponse(NetworkResponse response) {
+                    String responseString = "";
+                    if (response != null) {
+                        responseString = String.valueOf(response.statusCode);
+                    }
+                    return Response.success(responseString, HttpHeaderParser.parseCacheHeaders(response));
+                }
+            };
+
+            requestQueue.add(stringRequest);
+        } catch (JSONException e) {
+            e.printStackTrace();
         }
+    }
+
+    private void deleteData(final Dialog dialog, final String id) {
+        StringRequest request = new StringRequest(Request.Method.DELETE, url + "/" + id, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                dialog.dismiss();
+                refresh.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        fuelDetails.clear();
+                    }
+                });
+                Toast.makeText(context, "Record deleted successfully", Toast.LENGTH_LONG).show();
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(context, "Error occurred", Toast.LENGTH_LONG).show();
+            }
+        });
+        Volley.newRequestQueue(context).add(request);
     }
 
     @Override
